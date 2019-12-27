e9ce7cbaf5b1dfc93cded4eb04b86ceee85a48c7
uemuram <uemura.masaru1124@gmail.com>
2019-12-27 23:09:15 +0900

    レシピ作成中

--- src/App2.js
+++ src/App2.js
@@ -1,6 +1,66 @@
 import React from 'react';
 import './App.css';
 
+
+class Workflow extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+
+    this.state.flow = {
+      aaa : "123",
+      bbb : "234"
+    };
+
+    this.state.flow2 = {
+      title : "牛丼",
+      material : {
+        sugar : {
+          name : "砂糖",
+          quantity : [
+            {
+              amount : 9,
+              unit : "g"
+            }
+          ]
+        },
+        SoySauce : {
+          name : "醤油",
+          quantity : [
+            {
+              amount : 45,
+              unit : "ml"
+            }
+          ]
+        }
+      },
+      action : [
+        {
+          type : "move",
+          source : "sugar",
+          target : "bawl",
+          comment : "砂糖をボールへ"
+        },
+        {
+          type : "move",
+          source : "SoySauce",
+          target : "bawl",
+          comment : "醤油をボールへ"
+        }
+      ]
+    };
+
+
+
+  }
+
+  render() {
+    return (<div>{this.state.flow.bbb}</div>);
+  }
+}
+
 class App2 extends React.Component {
 
   constructor(props){
@@ -8,30 +68,28 @@ class App2 extends React.Component {
     this.state = {
       aaa: "abcdeffg",
     }
-//    alert("aaa: " + process.env.REACT_APP_BACKEND_URL  + " :bbb");
-
-//    fetch("http://localhost:5001/api/courses")
     fetch(process.env.REACT_APP_BACKEND_URL + "/api/courses")
-    .then(response => response.json())
-//      .then(json =>  alert(json[1].name));
+      .then(response => response.json())
       .then(json =>  this.setState( {aaa : json[0].name}));
-    }
+  }
+
+  renderWorkflow(){
+    return <Workflow />
+  }
 
   render() {
     return (
-      <p>
-      test4 : {this.state.aaa}
-    </p>
+      <div>
+        <p>
+          test4 : {this.state.aaa}
+        </p>
+        <div>
+          {this.renderWorkflow()}
+          {this.renderWorkflow()}
+        </div>
+      </div>
     )
   }
 }
-
-//function App2() {
-//  return (
-//    <p>
-//      test
-//    </p>
-//  );
-//}
 
 export default App2;
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App2 />, document.getElementById('root2'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
