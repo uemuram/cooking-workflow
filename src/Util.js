940af89c2322f9ed7bb32c520e62d687109023bd
uemuram <uemura.masaru1124@gmail.com>
2020-01-31 00:35:45 +0900

    次アクションの配列化に対応

--- src/Util.js
+++ src/Util.js
@@ -12,42 +12,72 @@ class Util {
         return "test3xx";
     };
 
+    // 配列判定
+    isArray(obj) {
+        return Object.prototype.toString.call(obj) === '[object Array]';
+    }
+
+    setHierarchy(recipe) {
+        console.log("a : " + recipe.title);
+        recipe.title = recipe.title + " abc";
+    }
+
     compileRecipe(recipe) {
         console.log(recipe.title);
         let compiledRecipe = Object.assign({}, recipe);
 
+        // 開始、終了ノードを作る
+        compiledRecipe.action.start = { next: [] };
+        compiledRecipe.action.finish = {};
+
         // ひとつ前のアクションへのリンクを各アクションに貼る
         let action = compiledRecipe.action
         for (let currentActionName in action) {
-            if (action.hasOwnProperty(currentActionName)) {
-                let nextActionName = action[currentActionName].next;
+            // 今見ているアクション
+            let currentAction = action[currentActionName];
+
+            // 階層をリセットしておく
+            currentAction.hierarchy = 0;
+
+            if (currentActionName === "finish") {
+                continue;
+            }
+            // 次アクションを配列化
+            if (!this.isArray(currentAction.next)) {
+                currentAction.next = [currentAction.next];
+            }
+
+            // 次アクションに対してループ
+            for (let i = 0; i < currentAction.next.length; i++) {
+                let nextActionName = currentAction.next[i];
                 console.log(currentActionName + " -> " + nextActionName);
-                // 次のアクションがfinishなら無視
-                if (nextActionName === "finish") {
-                    continue;
-                }
+
                 // 次アクションの実態がない場合はエラー
                 if (!action[nextActionName]) {
                     console.log("Error : 次アクションなし");
                     continue;
                 }
+
                 // 手前アクションをセット
                 if (!action[nextActionName].prev) {
                     action[nextActionName].prev = [currentActionName];
                 } else {
                     action[nextActionName].prev.push(currentActionName);
                 }
+
             }
         }
-        // 開始アクションを探す
+
+        // 開始アクションの次のアクションを探す
         for (let currentActionName in action) {
-            if (action.hasOwnProperty(currentActionName)) {
-                if(!action[currentActionName].prev){
-                    action[currentActionName].prev = "start";
-                }
+            if (!action[currentActionName].prev && currentActionName !== "start") {
+                action[currentActionName].prev = ["start"];
+                action.start.next.push(currentActionName);
             }
         };
 
+        this.setHierarchy(compiledRecipe);
+        console.log("b : " + compiledRecipe.title);
 
         return compiledRecipe;
     }
